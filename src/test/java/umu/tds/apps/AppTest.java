5eec398ad82722fdec14c59d4aa74aa8c5d92ec6
alessio-tds <alessio.celentano@um.es>
2025-01-09 15:57:57 +0100

    Add project structure

---
+++ src/test/java/umu/tds/apps/AppTest.java
@@ -0,0 +1,25 @@
+package umu.tds.apps;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+    /**
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    public void testApp() {
+        assertTrue( true );
+    }
+}
